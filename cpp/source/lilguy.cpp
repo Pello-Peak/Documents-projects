081bac125207650d9804a5693031b4ef8cac1021
Naion Pellchio <naionpellechio@gmail.com>
2025-08-11 20:23:03 -0400

    added emotion

--- cpp/source/lilguy.cpp
+++ cpp/source/lilguy.cpp
@@ -8,22 +8,46 @@
 
 using namespace std;
 atomic<int> food(4);
+atomic<int> emotionSet(2);
 string foodBar[5] = {" ","-", "- -", "- - -", "- - - -"};
+string emotion[3] = {";_;","*_*","^_^"};
 char ch = '\0';
 bool timerAct = true;
 
 void foodBarTimer() {
     random_device rd;
     mt19937 gen(rd());
-    uniform_int_distribution<> dis(10, 45);
+    uniform_int_distribution<> dis(5, 10);
 
     while (true) {
-        this_thread::sleep_for(chrono::minutes(dis(gen)));
+        this_thread::sleep_for(chrono::seconds(dis(gen)));
         food--;
     }
 }
 
-void death() {
+void emotionEval() {
+    while (true) {
+        switch(food) {
+        case 0:
+            emotionSet = 0;
+            break;
+        case 1:
+            emotionSet = 0;
+            break;
+        case 2:
+            emotionSet = 1;
+            break;
+        case 3:
+            emotionSet = 1;
+            break;
+        case 4:
+            emotionSet = 2;
+            break;
+        }
+    }
+}
+
+void deathCheck() {
     while (true) {
         if (food <= 0) {
             endwin();
@@ -41,7 +65,8 @@ int main() {
     nodelay(stdscr, TRUE);
 
     thread(foodBarTimer).detach();
-    thread(death).detach();
+    thread(deathCheck).detach();
+    thread(emotionEval).detach();
 
     while (true) {
         ch = getch();
@@ -51,9 +76,9 @@ int main() {
         }
 
         clear();
-        mvprintw(0, 0, "*_*");
-        mvprintw(2, 0, "[F]Feed");
-        mvprintw(4, 0, "Food: %s", foodBar[food].c_str());
+        mvprintw(0, 0, emotion[emotionSet].c_str());
+        mvprintw(4, 0, "[F]Feed");
+        mvprintw(6, 0, "Food: %s", foodBar[food].c_str());
         refresh();
         this_thread::sleep_for(chrono::milliseconds(250));
 
@@ -65,8 +90,8 @@ int main() {
 
         clear();
         mvprintw(0, 0, "-_-");
-        mvprintw(2, 0, "[F]Feed");
-        mvprintw(4, 0, "Food: %s", foodBar[food].c_str());
+        mvprintw(4, 0, "[F]Feed");
+        mvprintw(6, 0, "Food: %s", foodBar[food].c_str());
         refresh();
         this_thread::sleep_for(chrono::milliseconds(250));
     }
